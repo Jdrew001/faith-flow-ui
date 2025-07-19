a66552cd7a8d6569358eac606b118010ca96b1c0
Drew Atkison <dtatkison@gmail.com>
2025-07-18 19:54:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'faith-flow-ui',
+  webDir: 'www'
+};
+
+export default config;
