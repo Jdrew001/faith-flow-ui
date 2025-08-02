36b91427c2855c994381d730ff0165058466dcb4
Drew Atkison <dtatkison@gmail.com>
2025-08-02 13:51:22 -0500

    enviornment url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/faith-flow-service'
+  apiUrl: 'http://staging-faithflow.discoverfaitharlington.org/api/faith-flow-service'
 };
